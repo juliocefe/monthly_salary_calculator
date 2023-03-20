afac81907f9b6863496d18ffccee102209292999
juliocefe <juliocesarfelixhz95@gmail.com>
2023-03-19 21:20:53 -0700

    feature: add and list employees

--- monthly_salary_calculator/employees/views.py
+++ monthly_salary_calculator/employees/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.views import View
+from .forms import EmployeeForm
+from .models import Employee
 
 # Create your views here.
 class EmployeesView(View):
 
     def get(self, request):
-        return render(request, "employees/index.html")
+        context = {
+            "form": EmployeeForm(),
+            "employees": Employee.objects.all()
+        }
+        return render(request, "employees/index.html", context)
+        
+    def post(self, request):
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            # Save the new employee to the database
+            form.save()
+            messages.success(request, 'Empleado creado con exito!.')
+            # Redirect to the employee list page or some other page
+        return redirect('employees:')
