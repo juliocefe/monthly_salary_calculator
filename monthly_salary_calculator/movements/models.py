8fd9171f18ee5ea4a5b17e7d5737499b6773987f
juliocefe <juliocesarfelixhz95@gmail.com>
2023-03-19 22:18:43 -0700

    db: Movement model

--- monthly_salary_calculator/movements/models.py
+++ monthly_salary_calculator/movements/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Movement(models.Model):
+    employee = models.ForeignKey(
+        "employees.Employee",
+        verbose_name="Empleado",
+        on_delete=models.CASCADE,
+    )
+    deliveries = models.IntegerField("Entregas")
+    created_at = models.DateTimeField(
+        verbose_name="Fecha de creación",
+        default=timezone.now,
+        help_text="Fecha en la que el recurso fue creado.",
+    )
