9749f29557406b7d5d75109c922ea0cd1e7de9c9
juliocefe <juliocesarfelixhz95@gmail.com>
2023-03-20 18:58:56 -0700

    fix: protect home view

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", login_required(TemplateView.as_view(template_name="pages/home.html")), name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
